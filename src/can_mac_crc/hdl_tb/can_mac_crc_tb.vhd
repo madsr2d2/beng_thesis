bca75c3de6dbf2076b9fb7f143f34719565126b8
madsr2d2 <mads.richardt@gmail.com>
2026-05-08 13:23:20 +0200

    comment update in crc tb

--- src/can_mac_crc/hdl_tb/can_mac_crc_tb.vhd
+++ src/can_mac_crc/hdl_tb/can_mac_crc_tb.vhd
@@ -248,7 +248,7 @@ begin
 
     case rx_rec.Operation is
       when CHECK =>
-        -- REQ-009: CRC polynomial selection (CRC_15/17/21) and receiver verification
+        -- REQ-007: CRC polynomial selection (CRC_15/17/21) and receiver verification
         AffirmIf(crc_check_id, std_logic_vector(crc_o.crc) = std_logic_vector(rx_rec.DataToModel), "Got : " & to_string(crc_o.crc) & ", expected: " & to_string(std_logic_vector(rx_rec.DataToModel)));
         rx_rec.Ack <= rx_rec.Ack + 1;
       when others => null;
@@ -274,7 +274,7 @@ begin
         when others =>
           v_expected := (others => '0');
       end case;
-      -- REQ-010: CRC_INIT_VECTOR = (0,...,0) for CRC_15; (1,0,...,0) for CRC_17 and CRC_21
+      -- REQ-007: CRC_INIT_VECTOR = (0,...,0) for CRC_15; (1,0,...,0) for CRC_17 and CRC_21
       AffirmIf(reset_id, crc_o.crc = v_expected, "CRC output not reset to init value");
     end loop;
   end process p_reset_checker;
